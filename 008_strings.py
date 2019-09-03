d7f76c04c970e06323abdd4102d2e14936b07d1e
apanth <36489646+apanth@users.noreply.github.com>
2019-09-02 17:49:40 -0700

    Try changing a name to reverse.
    String usage.

---
+++ 008_strings.py
@@ -0,0 +1,21 @@
+"""
+strings Example
+
+author: Anirudh Panth
+"""
+
+#word = "Anirudh"
+word = "Loyd"
+print (word)
+#print (word[0])
+#print (word[4])
+#print (word[5])
+#print (word[4:6])
+#print (word[6] + word[5] + word[4] + word[3] + word[2] + word[1] + word[0])
+print (word[3] + word[2] + word[1] + word[0])
+
+
+
+
+
+
