1fc768d2dc9d1bd27b49934dad0be8b38c102312
apanth <36489646+apanth@users.noreply.github.com>
2019-09-02 17:42:58 -0700

    Sample file to draw a graph, connecting points
    Similar to connect the dots x,y as done in school with graph paper.

---
+++ 006_shapes.py
@@ -0,0 +1,40 @@
+"""
+shapes Example
+
+author: Anirudh Panth
+"""
+
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+plt.figure(figsize=(5,5))
+
+#x = [5, 10, 10, 5, 5]
+
+#y = [5, 5, 10, 10, 5]
+
+#plt.plot(x, y)
+
+#x = [15, 25, 25, 15, 15]
+
+#y = [4, 4, 10, 10, 4]
+
+#plt.plot(x, y)
+
+#x = [15, 25, 30, 20, 15]
+
+#y = [14, 14, 20, 20, 14]
+
+#plt.plot(x, y)
+
+
+x = [30, 15, 0, 15, 30]
+y = [0, 30, 0, 15, 0 ]
+plt.plot(x,y)
+
+plt.axis([0, 30, 0, 30])
+plt.show()
+
+
+
