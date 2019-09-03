f9c787d43cc8cd6abf2015366e82d4b7b6d7fabb
apanth <36489646+apanth@users.noreply.github.com>
2019-09-02 17:46:58 -0700

    Experiments with variables and loops with steps.
    Also try negative step.

---
+++ 007_loops.py
@@ -0,0 +1,42 @@
+"""
+loops Example
+
+author: Anirudh Panth
+"""
+
+#x = 10
+#print (x)
+
+#x = x - 1
+#print (x)
+
+#x = x - 1
+#print (x)
+
+#x = x - 1
+#print (x)
+
+#x = x - 1
+#print (x)
+
+#x = x - 1
+#print (x)
+
+#x = x - 1
+#print (x)
+
+#x = x - 1
+#print (x)
+
+#x = x - 1
+#print (x)
+
+#x = x - 1
+#print (x)
+
+#x = x - 1
+#print (x)
+
+
+for x in range(10, 0, -1):
+    print (x)
